d4ea65735d345fe981a478e2b289a40d1f7be2ff
thEo <mail.rezaulalam@gmail.com>
2023-07-03 23:33:44 +0600

    Project Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,97 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ContactListApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Contact {
+  final String name;
+  final String email;
+  final String phoneNumber;
 
-  // This widget is the root of your application.
+  Contact({required this.name, required this.email, required this.phoneNumber});
+}
+
+class ContactListApp extends StatelessWidget {
+  final List<Contact> contacts = [
+    Contact(
+      name: 'Khalid Ahmad',
+      email: 'khalid_ahmad123@gmail.com',
+      phoneNumber: '01950321456',
+    ),
+    Contact(
+      name: 'Arman Akter Sadi',
+      email: 'armanakter@gmail.com',
+      phoneNumber: '01792632514',
+    ),
+    Contact(
+      name: 'Mihal Kaykobad',
+      email: 'mihal.kaykobad@live.com',
+      phoneNumber: '01592367841',
+    ),
+  ];
+
+  ContactListApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Contact List',
       theme: ThemeData(
-
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ContactListScreen(contacts: contacts),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class ContactListScreen extends StatelessWidget {
+  final List<Contact> contacts;
 
-  final String title;
+  const ContactListScreen({super.key, required this.contacts});
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void _showContactDetails(BuildContext context, Contact contact) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                "Contact Details",
+                style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 8.0),
+              Text('Name: ${contact.name}', style: const TextStyle(fontSize: 18.0),),
+              const SizedBox(height: 8.0),
+              Text('Email: ${contact.email}', style: const TextStyle(fontSize: 18.0),),
+              const SizedBox(height: 8.0),
+              Text('Phone: ${contact.phoneNumber}', style: const TextStyle(fontSize: 18.0),),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+      appBar: AppBar(title: const Text('Contact List'), centerTitle: true,),
+      body: ListView.builder(
+        itemCount: contacts.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text(contacts[index].name),
+            onTap: () {
+              _showContactDetails(context, contacts[index]);
+            },
+          );
+        },
       ),
-      body: Center(
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
